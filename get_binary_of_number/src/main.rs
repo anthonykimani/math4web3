26c92a2e328d50e34f637988e46f4834e60cc3b1
anthony <kimaniantoni@gmail.com>
2024-05-17 19:07:57 +0300

    chore: commits updates to get_binary_of_number

---
+++ get_binary_of_number/src/main.rs
@@ -0,0 +1,28 @@
+use std::any::type_name;
+fn main() {
+ get_binary(234);
+}
+
+fn get_binary(number: i64) -> Vec<i64> {
+    let mut halve = number / 2;
+    let mut binary = vec![];
+
+    while halve != 0 {
+        if type_of(halve) ==  {
+            binary.push(0);
+        } else if type_of(halve) ==  {
+            binary.push(1);
+        } else {
+            println!("Type not integer or float");
+        }
+        halve / 2;
+    }
+    println!("The Binary of {} is {:?}",number, binary);
+    return binary;
+}
+
+
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
---
+++ get_binary_of_number/target/debug/deps/get_binary_of_number-a205d8adbb8388b7.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kiman/Development/web3clubs/math4web3/get_binary_of_number/target/debug/deps/get_binary_of_number-a205d8adbb8388b7: src/main.rs
+
+/mnt/c/Users/kiman/Development/web3clubs/math4web3/get_binary_of_number/target/debug/deps/get_binary_of_number-a205d8adbb8388b7.d: src/main.rs
+
+src/main.rs:
