fb0074f03d158302790a0dfe89cbdb5540ac0fba
anthony <kimaniantoni@gmail.com>
2024-05-17 16:17:55 +0300

    chore: updates euclidean algorithm with return

--- euclidean_algorithm_with_rust/src/main.rs
+++ euclidean_algorithm_with_rust/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    get_gcd(12,28);
+    get_gcd(3345679, 4276543);
 }
 
-fn get_gcd(num_one: i32, num_two: i32) {
+fn get_gcd(num_one: i32, num_two: i32) -> i32 {
     let mut gcd_a = num_one;
     let mut gcd_b = num_two;
 
@@ -16,9 +16,8 @@ fn get_gcd(num_one: i32, num_two: i32) {
             remainder = gcd_b - (gcd_a * divider);
         }
         println!(" The GCD of {} and {} is {}", num_one, num_two, gcd_a);
-    }
-
-    if gcd_b < gcd_a {
+        return gcd_a;
+    } else if gcd_b < gcd_a {
         let mut divider = (gcd_a / gcd_b) as i32;
         let mut remainder = gcd_a - (gcd_b * divider);
         while remainder != 0 {
@@ -28,5 +27,9 @@ fn get_gcd(num_one: i32, num_two: i32) {
             remainder = gcd_a - (gcd_b * divider);
         }
         println!(" The GCD of {} and {} is {}", num_one, num_two, gcd_b);
+        return gcd_b;
+    } else {
+        println!("numbers may not have a GCD");
+        return 0;
     }
 }
